ab83072d65e768e6d3ace8aeef9bf99e4f03bc18
whstruts <whstruts@163.com>
2020-07-24 17:00:27 +0800

    20200724 同步供应商品到亿诺服务器 hykx_rd.gysgoods

---
+++ src/main/java/hykx/ds/whkc/rabbitmq/TopicReceivergysgoods.java
@@ -0,0 +1,29 @@
+package hykx.ds.whkc.rabbitmq;
+
+import hykx.ds.whkc.bean.gysgoods;
+import hykx.ds.whkc.tools.JSONChange;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RabbitListener(queues = "topic.gysgoods")
+public class TopicReceivergysgoods {
+    @Autowired
+    private KhzlService khzlService;
+    @RabbitHandler
+    public void process(String message) throws IOException
+    {
+        int i_pos;
+        String s_json;
+        gysgoods gysgoods = new gysgoods();
+        i_pos = message.indexOf("{");
+        s_json = message.substring(i_pos);
+        gysgoods = (gysgoods) JSONChange.jsonToObj(gysgoods,s_json);
+        khzlService.Itogysgoods(gysgoods);
+        System.out.println("接收者 TopicReceiverGYS,"+s_json);
+    }
+}
