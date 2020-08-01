280352f23a980961686530da77ceb07385c6239e
whstruts <whstruts@163.com>
2020-08-01 13:03:01 +0800

    20200801 增加同步数据方法

--- src/main/java/hykx/ds/whkc/mapper/KhzlMappper.java
+++ src/main/java/hykx/ds/whkc/mapper/KhzlMappper.java
@@ -34,7 +34,7 @@ public interface KhzlMappper {
     @Insert("replace INTO hykx_rd.yzy_goods (goods_sn,goods_name,goods_number,market_price,dj,shop_price,is_on_sale,YPDM,CDMC,CDDM,GG,TXM,DW,JX,PZWH,BZ,ZBZ,YXQ,PH,ISRETAIL,PCH,SCRQ,goods_id_s) " +
             "select drugCode as goods_sn,spmch as goods_name,stock as goods_number,0 as market_price, " +
             "truncate(k_bbgwj*(select markUp from hykx_rd.lmsys where customNo = 'lm-bb'),2) as dj, " +
-            "truncate(k_bbgwj*(select markUp from hykx_rd.lmsys where customNo = 'lm-bb')*(select markUp from lmsys where customNo = 'bb-ysb'),2) as shop_price,  " +
+            "truncate(k_bbgwj*(select markUp from hykx_rd.lmsys where customNo = 'lm-bb')*(select markUp from hykx_rd.lmsys where customNo = 'bb-ysb'),2) as shop_price,  " +
             "case jingd  " +
             "WHEN 'A' THEN (case DAYOFWEEK(NOW()) when 7 then 1 when 1 then 1 else 0 end)  " +
             "WHEN 'B' THEN (case when HOUR(NOW()) > 7  and HOUR(NOW()) <18 then 0 else 1 end) " +
