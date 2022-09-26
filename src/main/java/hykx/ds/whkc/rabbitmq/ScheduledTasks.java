a284f886657e02ec4b431b22f19b7d7aad0236e5
代世平 <daishiping@prolog-int.com>
2022-09-27 03:25:49 +0800

    20220927 暂停用YYN华源接口同步商品数据给森涛

--- src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
+++ src/main/java/hykx/ds/whkc/rabbitmq/ScheduledTasks.java
@@ -21,58 +21,58 @@ public class ScheduledTasks {
     private KhzlService khzlService;
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedDelay = 1800*1000)
-    public void reportCurrentTimeYZYGOODS()throws Exception {
-
-        System.out.println("UPDATEYZYGOODS!");
-
-        String context = "UPDATEYZYGOODS";
-
-        String routeKey = "topic.yzygoods";
-
-        String exchange = "topicExchange";
-
-        context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
-
-        System.out.println("UPDATEYZYGOODS : " + context);
-
-        sender.send(context);//20200520 whstruts 库存发布
-
-        List<YZYGOODS> listsYZYGOODS = khzlService.getYZYGOODS();
-        for (int i = 0; i < listsYZYGOODS.size(); i++) {
-            YZYGOODS yzygoods = listsYZYGOODS.get(i);
-
-            System.out.println("GetYZYGOODS,Name:" + JSONChange.objToJson(yzygoods));
-
-            context = JSONChange.objToJson(yzygoods);
-
-            routeKey = "topic.yzygoods";
-
-            exchange = "topicExchange";
-
-            context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
-
-            System.out.println("sendYZYGOODS : " + context);
-
-            sender.send(context);//20200520 whstruts 库存发布
-        }
-        if(listsYZYGOODS.size()>20000) //新上架的商品超过一定数据才删除老的商品
-        {
-            System.out.println("DELETEYZYGOODS!");
-
-            context = "DELETEYZYGOODS";
-
-            routeKey = "topic.yzygoods";
-
-            exchange = "topicExchange";
-
-            context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
-
-            System.out.println("DELETEYZYGOODS : " + context);
-
-            sender.send(context);//20200520 whstruts 库存发布
-        }
-    }
+//    @Scheduled(fixedDelay = 1800*1000)
+//    public void reportCurrentTimeYZYGOODS()throws Exception {
+//
+//        System.out.println("UPDATEYZYGOODS!");
+//
+//        String context = "UPDATEYZYGOODS";
+//
+//        String routeKey = "topic.yzygoods";
+//
+//        String exchange = "topicExchange";
+//
+//        context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+//
+//        System.out.println("UPDATEYZYGOODS : " + context);
+//
+//        sender.send(context);//20200520 whstruts 库存发布
+//
+//        List<YZYGOODS> listsYZYGOODS = khzlService.getYZYGOODS();
+//        for (int i = 0; i < listsYZYGOODS.size(); i++) {
+//            YZYGOODS yzygoods = listsYZYGOODS.get(i);
+//
+//            System.out.println("GetYZYGOODS,Name:" + JSONChange.objToJson(yzygoods));
+//
+//            context = JSONChange.objToJson(yzygoods);
+//
+//            routeKey = "topic.yzygoods";
+//
+//            exchange = "topicExchange";
+//
+//            context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+//
+//            System.out.println("sendYZYGOODS : " + context);
+//
+//            sender.send(context);//20200520 whstruts 库存发布
+//        }
+//        if(listsYZYGOODS.size()>20000) //新上架的商品超过一定数据才删除老的商品
+//        {
+//            System.out.println("DELETEYZYGOODS!");
+//
+//            context = "DELETEYZYGOODS";
+//
+//            routeKey = "topic.yzygoods";
+//
+//            exchange = "topicExchange";
+//
+//            context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+//
+//            System.out.println("DELETEYZYGOODS : " + context);
+//
+//            sender.send(context);//20200520 whstruts 库存发布
+//        }
+//    }
 
     @Scheduled(fixedDelay = 60*10*1000) //10分钟
     public void CurrentTimeGToY()throws Exception{
